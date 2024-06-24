7f49285f1f9c6fe930a42667657f358abbb1b73e
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-06-24 18:40:22 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -16,8 +16,30 @@ class User(db.Model, UserMixin):
     genre = db.Column(db.String(20), nullable=True)
     generation = db.Column(db.String(20), nullable=True)
     recommendations = db.Column(db.Text, nullable=True)
+    posts = db.relationship('Post', backref='author', lazy=True)
+    songs = db.relationship('Song', backref='owner', lazy=True)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    artist = db.Column(db.String(100), nullable=False)
+    album = db.Column(db.String(100), nullable=False)
+    genre = db.Column(db.String(50), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Song('{self.title}', '{self.artist}')"
