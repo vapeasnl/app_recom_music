fa6f24a983b184dc05f1cbda4960bdaf96181561
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-06-24 18:46:06 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
-from app import create_app
+from app import create_app, db
 
 app = create_app()
 
+# Initialisation de la base de données
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
     app.run(debug=True)
-
