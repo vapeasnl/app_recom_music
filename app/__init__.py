c4716c2dfcfae569cabfb8a3f02b7c9aab9a50b0
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-06-24 18:32:10 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.login_message_category = 'info'
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+
+    from app.auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from app.main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
 
