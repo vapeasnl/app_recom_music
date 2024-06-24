58141b843a5dc2cb3f66bd5fefb51ba4443cff31
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-06-24 18:46:51 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from config import Config
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -9,19 +11,18 @@ login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 login_manager.login_message_category = 'info'
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_object('config.Config')
+    app.config.from_object(config_class)
 
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
 
-    from app.auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
-
     from app.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    return app
+    from app.auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
+    return app
