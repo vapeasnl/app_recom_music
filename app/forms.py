158603cc1ec346d9625c9403e9587639437e382a
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-06-24 18:32:45 +0200

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,40 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.models import User
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=50)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    message = TextAreaField('Message', validators=[DataRequired(), Length(min=10, max=500)])
+    submit = SubmitField('Send Message')
+
+class RecommendationForm(FlaskForm):
+    mood = SelectField('Mood', choices=[('happy', 'Happy'), ('sad', 'Sad'), ('energetic', 'Energetic'), ('calm', 'Calm')])
+    genre = SelectField('Genre', choices=[('rock', 'Rock'), ('pop', 'Pop'), ('jazz', 'Jazz'), ('classical', 'Classical')])
+    generation = SelectField('Generation', choices=[('60s', '60s'), ('70s', '70s'), ('80s', '80s'), ('90s', '90s')])
+    submit = SubmitField('Get Recommendation')
 
